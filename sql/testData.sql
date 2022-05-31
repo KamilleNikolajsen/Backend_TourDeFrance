6960d39c3c1804ff8a877901554f656c2f60248f
kamillenikolajsen <knikolajsen4@gmail.com>
2022-05-31 03:34:09 +0200

    Add finished scripts

--- sql/testData.sql
+++ sql/testData.sql
@@ -11,27 +11,8 @@ INSERT INTO cyclist(id, age, name, time_total_hours, mountain_points, sprint_poi
 
 INSERT INTO cyclist(id, age, name, time_total_hours, mountain_points, sprint_points, team_id) VALUES (4, 32, "Nina", 6.4, 3, 1, 2);
 INSERT INTO cyclist(id, age, name, time_total_hours, mountain_points, sprint_points, team_id) VALUES (5, 27, "Sine", 4.6, 5, 4, 2);
-INSERT INTO cyclist(id, age, name, time_total_hours, mountain_points, sprint_points, team_id) VALUES (2, 28, "Time", 8.9, 1, 0, 1);
+INSERT INTO cyclist(id, age, name, time_total_hours, mountain_points, sprint_points, team_id) VALUES (6, 28, "Filippa", 7.4, 1, 1, 2);
 
-VALUES
-(<{id: }>,
-    <{age: }>,
-    <{name: }>,
-    <{time_total_hours: }>,
-    <{mountain_points: }>,
-    <{sprint_points: }>,
-    <{team_id: }>);
-
-
-INSERT INTO `tourDeFrance`.`team`
-(`id`,
- `name`,
- `mountain_point_total`,
- `sprint_point_total`,
- `time_total`)
-VALUES
-(<{id: }>,
-    <{name: }>,
-    <{mountain_point_total: }>,
-    <{sprint_point_total: }>,
-    <{time_total: }>);
+INSERT INTO cyclist(id, age, name, time_total_hours, mountain_points, sprint_points, team_id) VALUES (7, 21, "Dolly", 4.8, 6, 3, 3);
+INSERT INTO cyclist(id, age, name, time_total_hours, mountain_points, sprint_points, team_id) VALUES (8, 24, "Lovis", 3.5, 8, 5, 3);
+INSERT INTO cyclist(id, age, name, time_total_hours, mountain_points, sprint_points, team_id) VALUES (9, 23, "Sanne", 9.2, 0, 0, 3);
